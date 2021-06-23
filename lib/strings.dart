a99e03a1f18cbc0db04e77fc98902f1d877fdfbf
aymen-benticha <aymen.benticha@enis.tn>
2021-06-23 10:43:52 +0100

    first

---
+++ lib/strings.dart
@@ -0,0 +1,10 @@
+String start = "Start";
+String statuePrefab = "statueModel";
+String cubePrefab = "cubeModel";
+String catPrefab = "catModel";
+String legoHousePrefab = "legoHouseModel";
+String aboutApp = "MyENISApp is a Flutter application which uses "
+    "Google's ARCore to spawn 3D models into the real world. "
+    "Additionally, it uses the Sceneform SDK to make it easier "
+    "to handle 3D models.";
+String repoLink = "https://github.com/aymen-benticha";
